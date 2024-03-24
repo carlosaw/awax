aca57f173b2e876393908875c156f79d722e02bf
carlosaw <carlosfreevg@gmail.com>
2024-03-24 17:48:02 -0400

    sliders

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function imgSlider1() {
+  document.querySelector("#slide1").style = "margin-left:0vw";
+} 
+function imgSlider2(){
+  document.querySelector("#slide1").style = "margin-left:-100vw"; 
+  
+}
+function imgSlider3(){
+  document.querySelector("#slide1").style = "margin-left:-200vw"; 
+  
+}
